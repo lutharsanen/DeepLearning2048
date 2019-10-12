7dc34852de81dada76d8395c2b253e410ba6b603
Lutharsanen Kunam <luthars@hotmail.com>
2019-10-12 15:34:20 +0200

    Update 2048.py

--- Python/2048.py
+++ Python/2048.py
@@ -1,33 +1,44 @@
 import gym_2048
 import gym
-
+import random 
+import numpy as np
 
 if __name__ == '__main__':
   env = gym.make('2048-v0')
 
   ########edited from Lu#############
-  max = 0
-  for i in range(1):
+  #max = 0
+  #for i in range(1):
   ###################################  
-    env.seed(i)
-    env.reset()
-    #env.render()
+  env.seed(random.randint(0,100))
+  env.reset()
+  #env.render()
 
-    done = False
-    moves = 0
-    while not done:
-      action = env.np_random.choice(range(4), 1).item()
-      print(action)
-      next_state, reward, done, info = env.step(action)
-      moves += 1
+  next_state = 0
+  done = False
+  moves = 0
+  action_set = [0,1,2,3]
+  while not done:
+    action = 1
+    prev_state = next_state
+    next_state, reward, done, info = env.step(action)
+    while np.array_equal(prev_state,next_state):
+      temp_action_set = action_set
+      print(temp_action_set)
+      break
+      #temp_action_set.remove(action)
+      #action = random.choice(temp_action_set)
+      #if temp_action_set == []:
+        #done = True 
+    moves += 1
 
-      #print('Next Action: "{}"\n\nReward: {}'.format(
-      #  gym_2048.Base2048Env.ACTION_STRING[action], reward))
-      #env.render()
+    print('Next Action: "{}"\n\nReward: {}'.format(
+      gym_2048.Base2048Env.ACTION_STRING[action], reward))
+    env.render()
 
     ########edited from Lu#############
-    if moves > max :
-      max = moves
+    #if moves > max :
+    #  max = moves
     ###################################
     
   #print('\nTotal Moves: {}'.format(max))
