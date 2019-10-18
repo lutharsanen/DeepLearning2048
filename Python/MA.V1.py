1131032c345808b64214281e79d29bdb0dbb773c
Braem513 <56351647+Braem513@users.noreply.github.com>
2019-10-18 14:05:45 +0200

    Update MA.V1.py

--- Python/MA.V1.py
+++ Python/MA.V1.py
@@ -12,11 +12,11 @@ i = random.randint(0,100)
 # MA: print not needed, but maybe save/log the generated seed for each episode? 
 print(i)
 env.seed(i)
-env.reset()
+state_init = env.reset()
 #MA: note, not always same start 
 # (sometimes only 1x "2", sometimes 2x "2", sometimes 1x"2" & 1x"4")
 # for moves_check at end
-sum_init = np.sum(env.reset())
+sum_init = np.sum(state_init)
 
 
 done = False
@@ -33,10 +33,12 @@ while not done:
 
 print('\nTotal Moves: {}'.format(moves))
 #MA check "sum of all tiles: np.sum(next_state)), sum_init line 19
-print(sum_init)
+print("Sum at beginning : ",sum_init)
 sum_end = np.sum(next_state)
-print(sum_end)
-sum_diff = sum_end - sum_init
+print("Sum at end: ", sum_end)
+sum_diff = np.subtract(sum_end,sum_init)
 print(sum_diff)
 moves_check = sum_diff/2
 print("There have been ", moves_check," successful moves")
+#MA: to check that sum_init is correct
+# print(state_init)
