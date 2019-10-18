df6a893e21522384182c808452b8806841f1f9b2
Braem513 <56351647+Braem513@users.noreply.github.com>
2019-10-18 13:42:13 +0200

    Update MA.V1.py

--- Python/MA.V1.py
+++ Python/MA.V1.py
@@ -15,9 +15,28 @@ env.seed(i)
 env.reset()
 #MA: note, not always same start 
 # (sometimes only 1x "2", sometimes 2x "2", sometimes 1x"2" & 1x"4")
+# for moves_check at end
+sum_init = np.sum(env.reset())
+
 
 done = False
 moves = 0
 while not done:
-    action = 0
+    print(env.action_space)
+    action = env.np_random.choice(range(4), 1).item()
     next_state, reward, done, info = env.step(action)
+    moves += 1
+
+    print('Next Action: "{}"\n\nReward: {}'.format(
+      gym_2048.Base2048Env.ACTION_STRING[action], reward))
+    env.render()
+
+print('\nTotal Moves: {}'.format(moves))
+#MA check "sum of all tiles: np.sum(next_state)), sum_init line 19
+print(sum_init)
+sum_end = np.sum(next_state)
+print(sum_end)
+sum_diff = sum_end - sum_init
+print(sum_diff)
+moves_check = sum_diff/2
+print("There have been ", moves_check," successful moves")
