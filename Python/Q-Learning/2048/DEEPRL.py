89802b292c5d632a1592ecdcad4832f5b8d95930
Lutharsanen Kunam <luthars@hotmail.com>
2019-11-26 22:34:19 +0100

    update

--- Python/Q-Learning/2048/DEEPRL.py
+++ Python/Q-Learning/2048/DEEPRL.py
@@ -9,34 +9,33 @@ env = gym.make('2048-v0')
 
 LEARNING_RATE = 0.001
 DISCOUNT = 0.95
-EPISODES = 250000
+EPISODES = 25000
 GOAL = 256
 
 SHOW_EVERY = 2000
 
-epsilon = 0.5
+epsilon = 0.8
 
 
 #should tinker with 20
-DISCRETE_OS_SIZE = [120] * len(env.observation_space.high)
+DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high-env.observation_space.low)/ DISCRETE_OS_SIZE
 
 
 #should tinker with -2 and 0
-q_table = np.random.uniform(low = -1,high = 2, size = (DISCRETE_OS_SIZE + [env.action_space.n]))
+q_table = np.random.uniform(low = -1,high = 1, size = (DISCRETE_OS_SIZE + [env.action_space.n]))
 
 def get_discrete_state(state):
     discrete_state = (state - env.observation_space.low)/discrete_os_win_size
-    return func.totuple(discrete_state)
+    return func.totuple(discrete_state.astype(np.int))
 
 myenv = env.reset()
 discrete_state = get_discrete_state(myenv)
 
 done = False
-counter = 0
 
 for episode in range(EPISODES):
-    episode_reward = 0
+    #episode_reward = 0
     
     while not done:
 
@@ -49,16 +48,9 @@ for episode in range(EPISODES):
         
         new_state, reward, done, _ = env.step(action)
         
-        print("action: " + str(action))
-        print("new state")
-        print(new_state)
-        
-        episode_reward += reward
+        #episode_reward += reward
         
         new_discrete_state = get_discrete_state(new_state)
-        
-        print("New Dis state")
-        print(new_discrete_state)
         
         if not done:
             max_future_q = np.max(q_table[new_discrete_state])
@@ -73,8 +65,8 @@ for episode in range(EPISODES):
             print("You won")
         discrete_state = new_discrete_state
     
-    if episode % SHOW_EVERY == 0:
-        print("Still here: " + str(episode))
+    #f episode % SHOW_EVERY == 0:
+        #print("Still here: " + str(episode))
 
     
 
