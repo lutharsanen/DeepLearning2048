5e8b7148cbf39e766771a3c1e4428532579cf21a
Lutharsanen Kunam <luthars@hotmail.com>
2019-12-04 14:10:24 +0100

    update

--- Python/Sarsa/functions.py
+++ Python/Sarsa/functions.py
@@ -16,16 +16,51 @@ def numpy_transformer(matrix):
                     lst.append(int(matrix[i][j]))
     return tuple(lst)
 
-def largest_number(env):
-    value = env.observation_space.high[0][0]
-    return int(value)
+def numpy_transformer_light(matrix):
+    lst = []
+    for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                    lst.append(int(matrix[i][j]))
+    return lst
+
 
 def get_max(state):
     max_value = max(state)
     return max_value
 
 
+def plotLearning(x, scores, epsilons, filename, lines=None):
+    fig=plt.figure()
+    ax=fig.add_subplot(111, label="1")
+    ax2=fig.add_subplot(111, label="2", frame_on=False)
 
+    ax.plot(x, epsilons, color="C0")
+    ax.set_xlabel("Game", color="C0")
+    ax.set_ylabel("Epsilon", color="C0")
+    ax.tick_params(axis='x', colors="C0")
+    ax.tick_params(axis='y', colors="C0")
+
+    N = len(scores)
+    running_avg = np.empty(N)
+    for t in range(N):
+	    running_avg[t] = np.mean(scores[max(0, t-20):(t+1)])
+
+    ax2.scatter(x, running_avg, color="C1")
+    #ax2.xaxis.tick_top()
+    ax2.axes.get_xaxis().set_visible(False)
+    ax2.yaxis.tick_right()
+    #ax2.set_xlabel('x label 2', color="C1")
+    ax2.set_ylabel('Score', color="C1")
+    #ax2.xaxis.set_label_position('top')
+    ax2.yaxis.set_label_position('right')
+    #ax2.tick_params(axis='x', colors="C1")
+    ax2.tick_params(axis='y', colors="C1")
+
+    if lines is not None:
+        for line in lines:
+            plt.axvline(x=line)
+
+    plt.savefig(filename)
 
 #f1 = open('states.txt', 'r')
 #mylist = ast.literal_eval(f1.read())
--- Python/Sarsa/sarsa.py
+++ Python/Sarsa/sarsa.py
@@ -13,55 +13,85 @@ if __name__ == '__main__':
     ALPHA = 0.1
     GAMMA = 0.9
     EPSILON = 1.0
+    #this variable counts how many times we have won.
     won = 0
+    #value we want to reach due to memory restrictions.
+    GOAL = 256
+    max_value = 0
 
     #construct state space
+    '''
     f1 = open('states.txt', 'r')
+    #we created the whole state_space externally to avoid a MemoryError. Though it was still hard for our machines.
     states = ast.literal_eval(f1.read())
     print("states created")
+    '''
 
-    
+    states = []
     Q = {}
+    '''
     for s in states:
         for a in range(4):
+            #we changed the state to a to avoid a TypeError, because lists aren't hashable.
             Q[tuple(s),a] = 0
+            '''
 
-    numGames = 50
-    totalRewards = np.zeros(numGames)
+    numGames = 900
+    totalRewards = []
     for i in range(numGames):
-        if i%5 == 0:
-            print('starting game', i)
-            game_won = False
-            observation = env.reset()
-            s = f.numpy_transformer(observation)
+        game_won = False
+        observation = f.numpy_transformer_light(env.reset())
+        s = tuple(observation)
+        #check if observation isn't already in states
+        if observation not in states:
+            states.append(observation)
+            for a in range(4):
+                #we changed the state to a to avoid a TypeError,
+                #because lists aren't hashable.
+                Q[tuple(s),a] = 0
+        rand = np.random.random()
+        if rand < (1-EPSILON):
+            a = f.maxAction(Q,s) 
+        else:
+            a = np.random.randint(0,4)
+        done = False
+        epRewards = 0
+        while not done:
+            new_state, reward, done, _ = env.step(a)
+            observation_ = f.numpy_transformer_light(new_state)
+            s_ = tuple(observation_)
+            if observation_ not in states:
+                states.append(observation_)
+                for a in range(4):
+                    #we changed the state to a to avoid a TypeError,
+                    #because lists aren't hashable.
+                    Q[tuple(s_),a] = 0
+
             rand = np.random.random()
             if rand < (1-EPSILON):
-                a = f.maxAction(Q,s) 
-            else:
-                a = np.random.randint(0,4)
-            done = False
-            epRewards = 0
-            while not done:
-                observation_, reward, done, _ = env.step(a)
-                s_ = f.numpy_transformer(observation_)
-                rand = np.random.random()
-                if rand < (1-EPSILON):
-                    a_ = f.maxAction(Q,s_)
-                else: 
-                    a_ = np.random.randint(0,4)
-                epRewards += reward
-                Q[s,a] = Q[s,a] + ALPHA*(reward + GAMMA*Q[s_,a_] - Q[s,a])
-                s,a = s_,a_
-                if f.get_max(s)>2048:
-                    game_won = True
+                a_ = f.maxAction(Q,s_)
+            else: 
+                a_ = np.random.randint(0,4)
+            epRewards += reward
+            Q[s,a] = Q[s,a] + ALPHA*(reward + GAMMA*Q[s_,a_] - Q[s,a])
+            s,a = s_,a_
+
+            #checks if the GOAL is reached. Sets the done to True to avoid KeyError (a higher state can be reached
+            # but we don't want to reach it, because we don't have in in our state_space.)
+            if f.get_max(observation_)==GOAL:
+                game_won = True
             
-            if game_won == True:
-                won+= 1
+            if f.get_max(observation_)>max_value:
+                max_value = f.get_max(observation_)
+        
+        if game_won == True:
+            won+= 1
 
-            if EPSILON > 0:
-                EPSILON -= 2/(numGames)  
-            else:
-                EPSILON = 0
-            totalRewards[i] = epRewards
-
+        if EPSILON > 0:
+            EPSILON -= 2/(numGames)  
+        else:
+            EPSILON = 0
+        totalRewards.append(epRewards)
+        if i%30 == 0:
             print("you won " + str(won) + " from " + str(i) + " episodes.")
+            print("MAX is:" + str(max_value))
