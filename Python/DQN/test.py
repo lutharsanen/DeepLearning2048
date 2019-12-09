ce536314772156805fcb73fe68db3df80a7382c9
Lutharsanen Kunam <luthars@hotmail.com>
2019-12-09 21:19:56 +0100

    Sarsa alpha=0.1 mono done

--- Python/DQN/test.py
+++ Python/DQN/test.py
@@ -1,28 +1,3 @@
-import gym
-import gym_2048
-import random
-import numpy as np
+test = [1,2,3,4,5,6]
 
-env = gym.make('2048-v0')
-env.reset()
-
-
-def numpy_transformer(matrix):
-    lst = []
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            lst.append(int(matrix[i][j]))
-    return lst
-
-
-
-done = False
-
-    
-while not done:
-    list
-    new_state, reward, done, _ = env.step(1)
-    print(new_state)
-    new = numpy_transformer(new_state)
-    print(type(new[0]))
-    break
+test1 = test[3:]
